13251f7b86a78bda36eaee04f66060d8ee587bd7
unverifiedchaos5017 <saifkhan501721@gmail.com>
2022-05-31 10:17:50 -0700

    first commit

---
+++ server/server.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import * as bodyParser from "body-parser"
+
+const app = express();
+
+const PORT:any = process.env.PORT || 4000
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.use(bodyParser.json())
+
+app.listen(() => {
+    console.log("Connected to port: " + PORT)
+})
